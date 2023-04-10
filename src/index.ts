122408c7407127a6283aa95bcdf0a289d0362cf4
hx2ryu <hx2ryu@gmail.com>
2023-04-10 22:57:55 +0900

    feat: export components

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export * from './components/Text';
+export * from './components/Avatar';
+export * from './components/Badge';
+export * from './components/Button';
 export * from './components/Layout';
+export * from './components/Text';
+export * from './theme';
