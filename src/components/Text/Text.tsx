4e765f266e6facb26271febef0b4f7436b6e7631
hx2ryu <hx2ryu@gmail.com>
2023-04-14 17:24:20 +0900

    refactor: allow text-color to accept string value

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -4,20 +4,16 @@ import type { Theme } from '@emotion/react';
 import { StyledText } from './Text.styles';
 
 export interface TextProps extends TextPropsBase {
-  varient?: keyof Theme['typography'];
-  language?: keyof Theme['fontFamily'];
-  color?: keyof Theme['colors'];
-  isBold?: boolean;
+    varient?: keyof Theme['typography'];
+    language?: keyof Theme['fontFamily'];
+    color?: string;
+    isBold?: boolean;
 }
 
-export const Text: React.FC<TextProps> = ({
-  isBold = false,
-  children,
-  ...props
-}) => {
-  return (
-    <StyledText isBold={isBold} {...props}>
-      {children}
-    </StyledText>
-  );
+export const Text: React.FC<TextProps> = ({ isBold = false, children, ...props }) => {
+    return (
+        <StyledText isBold={isBold} {...props}>
+            {children}
+        </StyledText>
+    );
 };
--- src/shared/styles.ts
+++ src/shared/styles.ts
@@ -5,29 +5,29 @@ import type { TextProps } from '../components/Text';
 type TypographyProps = TextProps & { theme: Theme };
 
 export const typographyStyle = (props: TypographyProps) => {
-  const { theme, varient, color, isBold, language } = props;
-  const { fontSize, fontWeight } = theme.typography[varient || 'body1'];
-  const {
-    bold: boldFont,
-    medium: mediumFont,
-    regular: regularFont,
-  } = theme.fontFamily[language || 'en'];
-  const isMedium = varient === 'subtitle1' || varient === 'subtitle2';
-  const isRegular =
-    varient === 'body1' ||
-    varient === 'body2' ||
-    varient === 'caption' ||
-    varient === undefined;
-  let fontFamily;
+    const { theme, varient, color, isBold, language } = props;
+    const { fontSize, fontWeight } = theme.typography[varient || 'body1'];
+    const {
+        bold: boldFont,
+        medium: mediumFont,
+        regular: regularFont,
+    } = theme.fontFamily[language || 'en'];
+    const isMedium = varient === 'subtitle1' || varient === 'subtitle2';
+    const isRegular =
+        varient === 'body1' ||
+        varient === 'body2' ||
+        varient === 'caption' ||
+        varient === undefined;
+    let fontFamily;
 
-  if (isBold) fontFamily = boldFont;
-  else if (isMedium) fontFamily = mediumFont;
-  else fontFamily = isRegular ? regularFont : boldFont;
+    if (isBold) fontFamily = boldFont;
+    else if (isMedium) fontFamily = mediumFont;
+    else fontFamily = isRegular ? regularFont : boldFont;
 
-  return css`
-    font-size: ${fontSize};
-    font-weight: ${isBold ? '600' : fontWeight};
-    color: ${theme.colors[color || 'gray200']};
-    font-family: ${fontFamily};
-  `;
+    return css`
+        font-size: ${fontSize};
+        font-weight: ${isBold ? '600' : fontWeight};
+        color: ${color || theme.colors.gray800};
+        font-family: ${fontFamily};
+    `;
 };
