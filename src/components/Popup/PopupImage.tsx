8de5121edb825c8e7f82d6d49fd1a64a6434c207
hx2ryu <hx2ryu@gmail.com>
2023-04-19 15:34:16 +0900

    refactor: use predefined styleProps of image

--- src/components/Popup/PopupImage.tsx
+++ src/components/Popup/PopupImage.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import type { ImageSourcePropType } from 'react-native';
+import type { ImageProps } from 'react-native';
 import { StyledPopupImage } from './Popup.styles';
 import type { RadiusType } from '../../shared';
 
-export interface PopupImageProps {
-    source: ImageSourcePropType;
+export interface PopupImageProps extends Omit<ImageProps, 'borderRadius'> {
     radius?: RadiusType;
-    width?: number | string;
-    height?: number;
-    aspectRatio?: number;
 }
 
-export const PopupImage: React.FC<PopupImageProps> = ({
-    radius,
-    source,
-    width = '100%',
-    height,
-    aspectRatio,
-}) => {
-    return (
-        <StyledPopupImage radius={radius} source={source} style={{ width, height, aspectRatio }} />
-    );
+export const PopupImage: React.FC<PopupImageProps> = (props) => {
+    return <StyledPopupImage {...props} />;
 };
