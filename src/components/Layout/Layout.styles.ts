6657dec4081ad5658e07ecfdb4e6b89ad4e7bbe0
hx2ryu <hx2ryu@gmail.com>
2023-04-19 15:40:20 +0900

    refactor: able to customize all of paddings

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -3,12 +3,16 @@ import { ScreenPadding } from '../../shared';
 import type { LayoutProps } from './Layout';
 
 export const LayoutContainer = styled.View<LayoutProps>`
-  padding: 0 16px;
+    padding: 0 16px;
 
-  ${(props) => {
-    return css`
-      flex-direction: ${props.orientation === 'horizontal' ? 'row' : 'column'};
-      padding: ${ScreenPadding[props.horrizontalPadding || 'None']};
-    `;
-  }}
+    ${(props) => {
+        const padding = ScreenPadding[props.topPadding || 'None'];
+        return css`
+            flex-direction: ${props.orientation === 'horizontal' ? 'row' : 'column'};
+            padding-top: ${props.topPadding ? ScreenPadding[props.topPadding] : padding};
+            padding-bottom: ${props.bottomPadding ? ScreenPadding[props.bottomPadding] : padding};
+            padding-left: ${props.leftPadding ? ScreenPadding[props.leftPadding] : padding};
+            padding-right: ${props.rightPadding ? ScreenPadding[props.rightPadding] : padding};
+        `;
+    }}
 `;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,49 +1,44 @@
-import React, {
-  Children,
-  cloneElement,
-  isValidElement,
-  ReactElement,
-} from 'react';
+import React, { Children, cloneElement, isValidElement, ReactElement } from 'react';
 import type { ViewProps, ViewStyle } from 'react-native';
 import type { Orientation, ScreenPaddingType } from '../../shared';
 import { LayoutContainer } from './Layout.styles';
 
 export interface LayoutProps extends ViewProps {
-  orientation?: Orientation;
-  spacing?: number;
-  horrizontalPadding?: ScreenPaddingType;
+    orientation?: Orientation;
+    spacing?: number;
+    padding?: ScreenPaddingType;
+    topPadding?: ScreenPaddingType;
+    bottomPadding?: ScreenPaddingType;
+    leftPadding?: ScreenPaddingType;
+    rightPadding?: ScreenPaddingType;
 }
 
 export const Layout: React.FC<LayoutProps> = ({
-  children,
-  orientation = 'horizontal',
-  spacing = 12,
-  ...props
+    children,
+    orientation = 'horizontal',
+    spacing = 12,
+    ...props
 }) => {
-  const childrenElements = Children.toArray(children)
-    .filter((element) => isValidElement(element))
-    .map((element, index, elements) => {
-      const reactElement = element as ReactElement;
-      const elementsLastIndex = elements.length - 1;
-      const contentStyle: ViewStyle = {
-        marginRight:
-          orientation === 'horizontal' && index < elementsLastIndex
-            ? spacing
-            : undefined,
-        marginBottom:
-          orientation === 'vertical' && index < elementsLastIndex
-            ? spacing
-            : undefined,
-      };
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      return cloneElement(reactElement, {
-        ...reactElement.props,
-        style: contentStyle,
-      });
-    });
-  return (
-    <LayoutContainer orientation={orientation} {...props}>
-      {childrenElements}
-    </LayoutContainer>
-  );
+    const childrenElements = Children.toArray(children)
+        .filter((element) => isValidElement(element))
+        .map((element, index, elements) => {
+            const reactElement = element as ReactElement;
+            const elementsLastIndex = elements.length - 1;
+            const contentStyle: ViewStyle = {
+                marginRight:
+                    orientation === 'horizontal' && index < elementsLastIndex ? spacing : undefined,
+                marginBottom:
+                    orientation === 'vertical' && index < elementsLastIndex ? spacing : undefined,
+            };
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+            return cloneElement(reactElement, {
+                ...reactElement.props,
+                style: contentStyle,
+            });
+        });
+    return (
+        <LayoutContainer orientation={orientation} {...props}>
+            {childrenElements}
+        </LayoutContainer>
+    );
 };
