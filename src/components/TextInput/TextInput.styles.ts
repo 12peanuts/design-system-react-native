034e07734d0f2001b76b5f9b88202578144cafae
hx2ryu <hx2ryu@gmail.com>
2023-04-20 20:32:18 +0900

    feat: implement Textinput

---
+++ src/components/TextInput/TextInput.styles.ts
@@ -0,0 +1,63 @@
+import styled, { css } from '@emotion/native';
+import { RadiusSize, typographyStyle } from '../../shared';
+import type { TextInputProps } from './TextInput';
+import { Text } from '../Text';
+import { Button } from '../Button';
+
+type TextInputContainerProps = Pick<TextInputProps, 'type' | 'radius'>;
+
+export const TextInputContainer = styled.View<TextInputContainerProps>`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: ${(props) => RadiusSize[props.radius || 'Medium']};
+    padding: 14px 16px;
+    margin: 8px 0;
+
+    ${(props) => {
+        const { colors } = props.theme;
+        switch (props.type) {
+            case 'contained':
+                return css`
+                    background-color: ${colors.gray300};
+                `;
+            case 'outlined':
+                return css`
+                    border: 1px solid ${colors.gray600};
+                `;
+            case 'underlined':
+            default:
+                return css`
+                    border-bottom-width: 0.5px;
+                    border-bottom-color: ${colors.gray600};
+                    padding: 9px 0;
+                    margin: 0 0 8px;
+                `;
+        }
+    }}
+`;
+
+export const TextInputTitleArea = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const TextInputHelpTextArea = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const Input = styled.TextInput`
+    ${typographyStyle};
+    flex: 1;
+    padding: 0;
+`;
+
+export const RequiredText = styled(Text)`
+    margin-left: 4px;
+`;
+
+export const ClearButton = styled(Button)`
+    margin: -16px -16px -16px 0;
+`;
---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { StyleProp, TextInputProps as TextInputPropsBase, View, ViewStyle } from 'react-native';
+import type { RadiusType } from '../../shared';
+import { Text, TextProps } from '../Text';
+import {
+    Input,
+    ClearButton,
+    RequiredText,
+    TextInputContainer,
+    TextInputHelpTextArea,
+    TextInputTitleArea,
+} from './TextInput.styles';
+import CleanIcon from '../../assets/images/ic-20-solid-clean.png';
+
+type TextInputType = 'contained' | 'outlined' | 'underlined';
+export interface TextInputProps extends TextInputPropsBase {
+    type?: TextInputType;
+    title?: string;
+    required?: boolean;
+    hasError?: boolean;
+    helpText?: string;
+    radius?: RadiusType;
+    style?: StyleProp<ViewStyle>;
+    textProps?: TextProps;
+    canClean?: boolean;
+}
+
+export const TextInput: React.FC<TextInputProps> = ({
+    title,
+    required,
+    helpText,
+    hasError,
+    type,
+    radius,
+    style,
+    canClean,
+    onChangeText,
+    textProps,
+    ...props
+}) => {
+    const handleChangeText = (text: string) => {
+        if (onChangeText) onChangeText(text);
+    };
+
+    return (
+        <View style={style}>
+            <TextInputTitleArea>
+                <Text isBold varient="h6">
+                    {title}
+                </Text>
+                {title && required && (
+                    <RequiredText isBold varient="h6" color="error">
+                        *
+                    </RequiredText>
+                )}
+            </TextInputTitleArea>
+            <TextInputContainer type={type} radius={radius}>
+                <Input onChangeText={handleChangeText} {...textProps} {...props} />
+                {canClean && (
+                    <ClearButton
+                        type="ghost"
+                        iconSrc={CleanIcon}
+                        onPress={() => handleChangeText('')}
+                    />
+                )}
+            </TextInputContainer>
+            <TextInputHelpTextArea>
+                <Text>{helpText}</Text>
+            </TextInputHelpTextArea>
+        </View>
+    );
+};
