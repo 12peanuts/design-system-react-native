0dc96316e3d80b3afd39e6334ab78ebda2663055
hx2ryu <hx2ryu@gmail.com>
2023-05-18 16:46:26 +0900

    refactor: enable to render based on aspect-ratio

--- src/components/Popup/PopupImage.tsx
+++ src/components/Popup/PopupImage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import type { ImageProps } from 'react-native';
-import { StyledPopupImage } from './Popup.styles';
+import { StyledImage } from './PopupImage.styles';
 import type { RadiusType } from '../../shared';
 
 export interface PopupImageProps extends Omit<ImageProps, 'borderRadius'> {
     radius?: RadiusType;
+    ratio?: number;
 }
 
 export const PopupImage: React.FC<PopupImageProps> = (props) => {
-    return <StyledPopupImage {...props} />;
+    return <StyledImage {...props} />;
 };
