03a35f783cef39e7283f39738b48f89e635181a2
hx2ryu <hx2ryu@gmail.com>
2023-05-25 15:46:43 +0900

    chore: add reanimated dependencies

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const pak = require('../package.json');
+import { join } from 'path';
+import { name, source } from '@12peanuts/design-system-react-native/package.json';
 
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
+export const presets = ['module:metro-react-native-babel-preset'];
+export const plugins = [
     [
-      'module-resolver',
-      {
-        extensions: ['.tsx', '.ts', '.js', '.json'],
-        alias: {
-          [pak.name]: path.join(__dirname, '..', pak.source),
+        'module-resolver',
+        {
+            extensions: ['.tsx', '.ts', '.js', '.json'],
+            alias: {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+                [name]: join(__dirname, '..', source),
+            },
         },
-      },
     ],
-  ],
-};
+    'react-native-reanimated/plugin',
+];
