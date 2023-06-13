c9745b8c4fc9121406cba520c08cb2714b4e90a9
hx2ryu <hx2ryu@gmail.com>
2023-06-13 17:20:35 +0900

    refactor: export default colors

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,28 +3,28 @@ import type { ColorSchemeName } from 'react-native';
 
 export interface DefaultTheme {
     colors: {
-        primary?: string;
-        secondary?: string;
-        tertiary?: string;
-        inactive?: string;
-        background?: string;
-        text100?: string;
-        text200?: string;
-        text300?: string;
-        gray100?: string;
-        gray150?: string;
-        gray200?: string;
-        gray300?: string;
-        gray350?: string;
-        gray400?: string;
-        gray500?: string;
-        gray600?: string;
-        gray700?: string;
-        gray800?: string;
-        gray900?: string;
-        white?: string;
-        black?: string;
-        error?: string;
+        primary: string;
+        secondary: string;
+        tertiary: string;
+        inactive: string;
+        background: string;
+        text100: string;
+        text200: string;
+        text300: string;
+        gray100: string;
+        gray150: string;
+        gray200: string;
+        gray300: string;
+        gray350: string;
+        gray400: string;
+        gray500: string;
+        gray600: string;
+        gray700: string;
+        gray800: string;
+        gray900: string;
+        white: string;
+        black: string;
+        error: string;
     };
     typography: {
         h1: {
@@ -86,7 +86,7 @@ export interface DefaultTheme {
     };
 }
 
-const defaultColors: {
+export const defaultColors: {
     light: DefaultTheme['colors'];
     dark: DefaultTheme['colors'];
 } = {
@@ -193,10 +193,7 @@ export function getDefaultTheme(
 ): Theme {
     const colors = defaultColors[systemTheme ?? 'dark'];
     return {
-        colors: {
-            ...colors,
-            ...additionalProps?.colors,
-        },
+        colors,
         typography: defaultTypography,
         fontFamily: {
             en: {
