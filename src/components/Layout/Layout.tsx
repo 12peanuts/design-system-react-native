09da92dc2b58ed51237cdc9a22606932555313b9
hx2ryu <hx2ryu@gmail.com>
2023-06-13 13:41:43 +0900

    feat: use gap property as spacing

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,46 +1,29 @@
-import React, { Children, cloneElement, isValidElement, ReactElement } from 'react';
-import type { ViewProps, ViewStyle } from 'react-native';
+import React from 'react';
+import type { ViewProps } from 'react-native';
 import type { Orientation, ScreenPaddingType } from '../../shared';
 import { LayoutContainer } from './Layout.styles';
 
 export interface LayoutProps extends ViewProps {
-    orientation?: Orientation;
-    spacing?: number;
     paddingAll?: ScreenPaddingType;
     paddingTop?: ScreenPaddingType;
     paddingBottom?: ScreenPaddingType;
     paddingLeft?: ScreenPaddingType;
     paddingRight?: ScreenPaddingType;
+    orientation?: Orientation;
+    spacing?: number;
     flex?: number;
 }
 
 export const Layout: React.FC<LayoutProps> = ({
     children,
-    orientation = 'vertical',
+    flex = undefined,
     spacing = 12,
+    orientation = 'vertical',
     ...props
 }) => {
-    const childrenElements = Children.toArray(children)
-        .filter((element) => isValidElement(element))
-        .map((element, index, elements) => {
-            const reactElement = element as ReactElement;
-            const elementsLastIndex = elements.length - 1;
-            const contentStyle: ViewStyle = {
-                marginRight:
-                    orientation === 'horizontal' && index < elementsLastIndex ? spacing : 0,
-                marginBottom: orientation === 'vertical' && index < elementsLastIndex ? spacing : 0,
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-                ...reactElement.props.style,
-            };
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-            return cloneElement(reactElement, {
-                ...reactElement.props,
-                style: contentStyle,
-            });
-        });
     return (
-        <LayoutContainer orientation={orientation} {...props}>
-            {childrenElements}
+        <LayoutContainer orientation={orientation} flex={flex} spacing={spacing} {...props}>
+            {children}
         </LayoutContainer>
     );
 };
